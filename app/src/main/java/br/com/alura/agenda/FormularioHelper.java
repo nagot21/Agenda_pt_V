befba3e058a32bb4e2cb949837fd4d41894018ef
Ian Nagot <inagot21@gmail.com>
2016-07-27 15:44:09 +0100

    First Commit

---
+++ app/src/main/java/br/com/alura/agenda/FormularioHelper.java
@@ -0,0 +1,45 @@
+package br.com.alura.agenda;
+
+import android.widget.EditText;
+import android.widget.RatingBar;
+
+import br.com.alura.agenda.modelo.Aluno;
+
+/**
+ * Created by Nagot on 19/02/2016.
+ */
+public class FormularioHelper {
+    private EditText campoNome;
+    private EditText campoEndereco;
+    private EditText campoTelefone;
+    private EditText campoSite;
+    private RatingBar campoNota;
+    private Aluno aluno;
+
+    FormularioHelper(FormularioActivity activity){
+        campoNome = (EditText) activity.findViewById(R.id.formulario_nome);
+        campoEndereco = (EditText) activity.findViewById(R.id.formulario_endereco);
+        campoTelefone = (EditText) activity.findViewById(R.id.formulario_telefone);
+        campoSite = (EditText) activity.findViewById(R.id.formulario_site);
+        campoNota = (RatingBar) activity.findViewById(R.id.formulario_nota);
+        aluno = new Aluno();
+    }
+
+    public Aluno pegaAluno() {
+        aluno.setNome(campoNome.getText().toString());
+        aluno.setEndereco(campoEndereco.getText().toString());
+        aluno.setTelefone(campoTelefone.getText().toString());
+        aluno.setSite(campoSite.getText().toString());
+        aluno.setNota(Double.valueOf(campoNota.getProgress()));
+        return aluno;
+    }
+
+    public void preencheFormulario(Aluno aluno) {
+        campoNome.setText(aluno.getNome());
+        campoEndereco.setText(aluno.getEndereco());
+        campoTelefone.setText(aluno.getTelefone());
+        campoSite.setText(aluno.getSite());
+        campoNota.setProgress(aluno.getNota().intValue());
+        this.aluno = aluno;
+    }
+}
