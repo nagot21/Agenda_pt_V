7a2421fd8b41ca3371a383c2ec3148d56cd22f18
Ian Nagot <inagot21@gmail.com>
2016-08-04 10:48:19 +0100

    Add number check on AlunoDAO and play a different sound if the sms is from any student

--- app/src/main/java/br/com/alura/agenda/receiver/SMSReceiver.java
+++ app/src/main/java/br/com/alura/agenda/receiver/SMSReceiver.java
@@ -3,7 +3,13 @@ package br.com.alura.agenda.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.telephony.SmsMessage;
 import android.widget.Toast;
+
+import br.com.alura.agenda.R;
+import br.com.alura.agenda.dao.AlunoDAO;
+
 
 /**
  * Created by Nagot on 04/08/2016.
@@ -11,6 +17,20 @@ import android.widget.Toast;
 public class SMSReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context, "Chegou um SMS!", Toast.LENGTH_SHORT).show();
+        Object[] pdus = (Object[]) intent.getSerializableExtra("pdus");
+        byte[] pdu = (byte[]) pdus[0];
+        String formato = (String) intent.getSerializableExtra("format");
+
+        SmsMessage sms = SmsMessage.createFromPdu(pdu, formato);
+
+        String telefone = sms.getDisplayOriginatingAddress();
+        AlunoDAO dao = new AlunoDAO(context);
+
+        if (dao.ehAluno(telefone)) {
+            Toast.makeText(context, "Chegou um SMS!", Toast.LENGTH_SHORT).show();
+            MediaPlayer mp = MediaPlayer.create(context, R.raw.msg);
+            mp.start();
+        }
+        dao.close();
     }
 }
